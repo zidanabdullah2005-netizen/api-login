0a6addd44ecfd0c4821c7d494fa3f117cc5fe67b
zidanabdullah2005-netizen <zidanabdullah2005@gmail.com>
2026-07-15 15:01:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -6,7 +6,7 @@ $host     = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $port     = getenv('MYSQLPORT') ?: "3306"; 
 $dbname   = getenv('MYSQLDATABASE') ?: "railway";                               
 $username = getenv('MYSQLUSER') ?: "root";                                  
-$password = getenv('MYSQLPASSWORD') ?: "qbwgyLhREkhOHVWIXnqCWrAxkTrynrsJ"; 
+$password = getenv('MYSQLPASSWORD') ?: "OARIoYZTZCTmpYkfeciEjIlgahJwkGYT"; 
 
 // Membuat koneksi
 try {
