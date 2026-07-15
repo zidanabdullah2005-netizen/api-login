3ac64a75266844e732156a59993a34d490cfa420
zidanabdullah2005-netizen <zidanabdullah2005@gmail.com>
2026-07-15 14:50:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Server Online";
+include 'koneksi.php';
+// index.php - Entry point for the application
+// Route requests to the appropriate PHP file
+
+$request = $_SERVER['REQUEST_URI'];
+$path = parse_url($request, PHP_URL_PATH);
+
+switch ($path) {
+    case '/daftar':
+    case '/daftar.php':
+        require 'daftar.php';
+        break;
+    default:
+        echo "API is running.";
+        break;
+}
 ?>
